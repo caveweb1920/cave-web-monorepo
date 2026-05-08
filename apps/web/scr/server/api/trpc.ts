27421b455c98dce1cf9bf30685dda0b1beb8a0dd
caveweb1920 <shineert35@gmail.com>
2026-05-09 01:18:21 +0600

    Create trpc.ts

---
+++ apps/web/scr/server/api/trpc.ts
@@ -0,0 +1,24 @@
+import { initTRPC } from "@trpc/server";
+import superjson from "superjson";
+import { ZodError } from "zod";
+import { prisma } from "../db";
+
+export const createTRPCContext = () => {
+  return { prisma };
+};
+
+const t = initTRPC.context<typeof createTRPCContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError: error.cause instanceof ZodError ? error.cause.flatten() : null,
+      },
+    };
+  },
+});
+
+export const router = t.router;
+export const publicProcedure = t.procedure;
