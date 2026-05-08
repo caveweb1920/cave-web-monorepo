a9a963e1704602820f064d7c47fba838f3858a3d
caveweb1920 <shineert35@gmail.com>
2026-05-09 01:15:01 +0600

    Create next.config.js

---
+++ apps/web/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ["@cave-web/ui", "@cave-web/utils"],
+};
+
+module.exports = nextConfig;
